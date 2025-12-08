2dd4d3535fc7e179b7b8953ced36579d282aed95
asitha26551 <asithamadumal26551@gmail.com>
2025-12-08 19:35:26 +0530

    Apply backend fixes: server and mongodb/cloudinary updates

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // App config
 const app = express()
