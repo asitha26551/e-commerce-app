0010df0b4362c61f5074df323839cd2e07b34ef7
asitha26551 <asithamadumal26551@gmail.com>
2025-12-16 08:48:44 +0530

    feat(admin): implement admin authentication

--- backend/modules/products/product.route.js
+++ backend/modules/products/product.route.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import { addProduct, getAllProducts, removeProductById, getProductById } from './product.controller.js';
 import upload from '../../middleware/multer.js';
+import { adminAuth } from '../../middleware/auth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/',upload.fields([
+productRouter.post('/',adminAuth,upload.fields([
     {name:'image1', maxCount:1}, 
     {name:'image2', maxCount:1},
     {name:'image3', maxCount:1}, 
     {name:'image4', maxCount:1}]
 ), addProduct);               
-productRouter.get('/', getAllProducts);            
-productRouter.get('/:id', getProductById);         
-productRouter.delete('/:id', removeProductById);  
+productRouter.get('/', adminAuth, getAllProducts);            
+productRouter.get('/:id', adminAuth, getProductById);         
+productRouter.delete('/:id', adminAuth, removeProductById);  
 
 export default productRouter;
